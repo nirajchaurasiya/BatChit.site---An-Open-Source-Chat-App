49222d6d50ce81a509b9a1aaaa8546b3f9d5534b
Niraj <developer.nirajchaurasiya@gmail.com>
2024-04-06 15:00:36 +0545

    main backend crash issue fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -78,7 +78,6 @@ io.on("connection", async (socket) => {
       if (socketId) {
          io.to(socketId).emit("add-an-online-user", user);
       }
-      cd;
    });
 
    socket.on("get-online-users", (userId) => {
